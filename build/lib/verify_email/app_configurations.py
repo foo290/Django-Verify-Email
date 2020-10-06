483db7cedd4bbb7cac20c3a0fb0dd763e2c4f9c5
Nitin Sharma <ns290670@gmail.com>
2020-10-06 11:14:04 +0530

    Migrated verification function in views, failed redairect link removed

---
+++ build/lib/verify_email/app_configurations.py
@@ -0,0 +1,120 @@
+from django.conf import settings
+
+
+class GetFieldFromSettings:
+    """
+    This class fetches the attributes that are defined in settings.py of your project by user OR Django itself.
+    self.default_configs : is a dict with keys as the names used in this app and values being a tuple of 
+                           attributes defined in settings.py and their corresponding default values if not found.
+    
+    There is a special case in "get" method, if you set "VERIFICATION_SUCCESS_TEMPLATE" as None is settings.py, it 
+    will skip the intermidiate page where success information is displayed. (This is better explained in docs.)
+
+    The "get" method takes the name of the attributes as input, checks for it in settings.py, 
+            if found:
+                returns the corresponding value.
+            else:
+                returns the default value from "self.defaults_configs".
+    """
+
+    def __init__(self):
+
+        self.defaults_configs = {
+            'debug_settings': (
+                'DEBUG', 
+                False
+            ),
+
+            'subject': (
+                "SUBJECT", 
+                "Email Verification Mail"
+            ),
+
+            'email_field_name': (
+                "EMAIL_FIELD_NAME", 
+                "email",
+            ),
+
+            'html_message_template': (
+                "HTML_MESSAGE_TEMPLATE", 
+                'verify_email/email_verification_msg.html'
+            ),
+
+            'from_alias': (
+                "DEFAULT_FROM_EMAIL", 
+                'noreply<noreply@gmail.com>',
+            ),
+
+            'verification_success_redirect': (
+                'LOGIN_URL',
+                'accounts_login'
+            ),
+
+            'verification_success_template': (
+                'VERIFICATION_SUCCESS_TEMPLATE',
+                'verify_email/email_verification_successful.html'
+            ),
+
+            'verification_success_msg': (
+                'VERIFICATION_SUCCESS_MSG',
+                "Your Email is verified successfully and account has been activated. You can login with the credentials now..."
+            ),
+
+            'verification_failed_template': (
+                'VERIFICATION_FAILED_TEMPLATE',
+                'verify_email/email_verification_failed.html'
+            ),
+
+            'verification_failed_msg': (
+                'VERIFICATION_FAILED_MSG',
+                "There is something wrong with this link, can't verify the user..."
+            ),
+        }
+
+    def get(self, field_name, raise_exception=True, default_type=str):
+        attr = getattr(
+            settings, 
+            self.defaults_configs[field_name][0],  # get field from settings
+            self.defaults_configs[field_name][1]   # get default value if field not defined
+        )
+        if (attr == '' or attr is None or not isinstance(field_name, default_type)) and raise_exception:
+            if field_name == 'verification_success_template' and attr is None:
+                return None
+            raise AttributeError
+        return attr
+
+
+        
+            
+        
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ build/lib/verify_email/email_handler.py
@@ -0,0 +1,69 @@
+from django.core.mail import BadHeaderError, send_mail
+from django.contrib.sites.shortcuts import get_current_site
+from base64 import urlsafe_b64encode
+from django.contrib.auth.tokens import default_token_generator
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from smtplib import SMTPException
+from .app_configurations import GetFieldFromSettings
+
+
+class _VerifyEmail:
+    """
+    This class does four things:
+    1. creates tokens for each user.
+    2. set each user as inactive and saves it
+    3. embed encoded token with encoded email to make verification link.
+    4. sends the email to user with that link.
+    """
+
+    def __init__(self):
+        self.settings = GetFieldFromSettings()
+
+    def __get_hashed_token(self, user):
+        return urlsafe_b64encode(str(default_token_generator.make_token(user)).encode('utf-8')).decode('utf-8')
+
+    def __make_verification_url(self, current_site, inactive_user, useremail):
+        token = self.__get_hashed_token(inactive_user)
+        email_enc = urlsafe_b64encode(str(useremail).encode('utf-8')).decode('utf-8')
+        link = f"{current_site}/verification/user/verify-email/{email_enc}/{token}/"
+
+        return link
+
+    def send_verification_link(self, request, form):
+        inactive_user = form.save(commit=False)
+        inactive_user.is_active = False
+        inactive_user.save()
+        try:
+            current_site = get_current_site(request)
+            try:
+                useremail = form.cleaned_data[self.settings.get('email_field_name')]
+            except:
+                raise KeyError(
+                    'No key named "email" in your form. Your field should be named as email in form OR set a variable'
+                    ' "EMAIL_FIELD_NAME" with the name of current field in settings.py if you want to use current name '
+                    'as email field.'
+                )
+
+            verification_url = self.__make_verification_url(current_site, inactive_user, useremail)
+            subject = self.settings.get('subject')
+            msg = render_to_string(self.settings.get('html_message_template', raise_exception=True),
+                                   {"link": verification_url})
+
+            try:
+                send_mail(subject, strip_tags(msg), from_email=self.settings.get('from_alias'),
+                          recipient_list=[useremail], html_message=msg)
+                return True
+            except (BadHeaderError, SMTPException):
+                inactive_user.delete()
+                return False
+
+        except Exception as error:
+            inactive_user.delete()
+            if self.settings.get('debug_settings'):
+                raise Exception(error)
+
+
+#  These is supposed to be called outside of this module
+def send_verification_email(request, form):
+    return _VerifyEmail().send_verification_link(request, form)
