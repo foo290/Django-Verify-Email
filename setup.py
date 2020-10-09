021f8fc470c551fa426d1ea96afd19ef61ef4007
Nitin <32377892+foo290@users.noreply.github.com>
2020-10-09 10:13:43 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Django-Verify-Email-2",
+    name="Django-Verify-Email-3",
     version="0.0.1",
     author="Nitin",
     author_email="ns290670@gamil.com",
