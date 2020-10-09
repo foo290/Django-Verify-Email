eb2e44572262dfb7e04a18d7fa4cb0d149db2185
Nitin Sharma <ns290670@gmail.com>
2020-10-09 11:50:16 +0530

    manifest added

--- setup.py
+++ setup.py
@@ -4,22 +4,6 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="Django-Verify-Email",
-    version="0.0.1",
-    author="Nitin",
-    author_email="ns290670@gamil.com",
-    description="A Django app for email verification.",
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/foo290/Django-Verify-Email/",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Environment :: Web Environment",
-        "Framework :: Django CMS :: 3.8",
-        "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3.6",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
+    long_description_content_type="text/markdown",    
 )
