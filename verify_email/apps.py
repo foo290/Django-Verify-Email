d7ed5c2adb5329ab2cf1a18b2b1c33c966350805
Haydn Greatnews <haydngreatnews@gmail.com>
2024-04-04 18:36:09 +1300

    Explicitly set default auto field
    Leaving this unset was making it incompatible with any project that had set the DEFAULT_AUTO_FIELD django setting to anything else

--- verify_email/apps.py
+++ verify_email/apps.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 
 class VerifyEmailConfig(AppConfig):
     name = 'verify_email'
+    default_auto_field = "django.db.models.BigAutoField"
 
     def ready(self):
         logger.info('[Email Verification] : importing signals    - OK.')
