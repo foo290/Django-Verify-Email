82713662553d67991c6305bdf85f28d5fae4a8a8
Olivier LEVILLAIN <olivier.levillain@free.fr>
2024-08-09 21:15:34 +0200

    1rst try to modernize the packaging

--- setup.py
+++
@@ -1,9 +0,0 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    long_description=long_description,
-    long_description_content_type="text/markdown",    
-)
