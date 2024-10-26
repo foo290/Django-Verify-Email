a1920eaacaf7fffe12d50ec7dc9148d784498267
foo290 <ns290670@gmail.com>
2024-10-26 10:12:40 +0530

    code format in urls

--- verify_email/urls.py
+++ verify_email/urls.py
@@ -2,7 +2,19 @@ from django.urls import path
 from .views import verify_user_and_activate, request_new_link
 
 urlpatterns = [
-    path(f'user/verify-email/<useremail>/<usertoken>/', verify_user_and_activate, name='verify-email'),
-    path(f'user/verify-email/request-new-link/<useremail>/<usertoken>/', request_new_link, name='request-new-link-from-token'),
-    path(f'user/verify-email/request-new-link/', request_new_link, name='request-new-link-from-email'),
+    path(
+        'user/verify-email/<useremail>/<usertoken>/',
+        verify_user_and_activate,
+        name='verify-email'
+    ),
+    path(
+        'user/verify-email/request-new-link/<useremail>/<usertoken>/',
+        request_new_link,
+        name='request-new-link-from-token'
+    ),
+    path(
+        'user/verify-email/request-new-link/',
+        request_new_link,
+        name='request-new-link-from-email'
+    ),
 ]
